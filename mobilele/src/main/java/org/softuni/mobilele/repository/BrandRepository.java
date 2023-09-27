1b79e1b853160d96b397acad65c1a396210a659b
rd-lachezar-balev <lachezar.balev@gmail.com>
2023-09-27 22:24:05 +0300

    Bug fix

--- mobilele/src/main/java/org/softuni/mobilele/repository/BrandRepository.java
+++ mobilele/src/main/java/org/softuni/mobilele/repository/BrandRepository.java
@@ -1,9 +1,10 @@
 package org.softuni.mobilele.repository;
 
+import org.softuni.mobilele.model.entity.BrandEntity;
 import org.softuni.mobilele.model.entity.OfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BrandRepository extends JpaRepository<OfferEntity, Long> {
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
 }
