f0ab4440878b0e17de5e90bc708ea277c97221ed
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2023-10-03 13:08:55 +0300

    getAllBrands@BrandServiceImpl refactored to one functional call

--- mobilele/src/main/java/org/softuni/mobilele/repository/ModelRepository.java
+++ mobilele/src/main/java/org/softuni/mobilele/repository/ModelRepository.java
@@ -4,7 +4,11 @@ import org.softuni.mobilele.model.entity.ModelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
 
+
+    List<ModelEntity> findAllByBrandId (Long id);
 }
