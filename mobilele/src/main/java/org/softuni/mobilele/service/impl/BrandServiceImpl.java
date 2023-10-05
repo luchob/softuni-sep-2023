fafa70ea4303440a7f591c4192332cedd75a9e26
Trayan Ivanov <101820581+thrako@users.noreply.github.com>
2023-10-05 11:35:39 +0300

    BrandServiceImpl.getAllBrands@mobilele: N + 1 problem addressed. Custom sorting provided.

--- mobilele/src/main/java/org/softuni/mobilele/service/impl/BrandServiceImpl.java
+++ mobilele/src/main/java/org/softuni/mobilele/service/impl/BrandServiceImpl.java
@@ -1,10 +1,13 @@
 package org.softuni.mobilele.service.impl;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.softuni.mobilele.model.dto.BrandDTO;
 import org.softuni.mobilele.model.dto.ModelDTO;
+import org.softuni.mobilele.model.entity.ModelEntity;
 import org.softuni.mobilele.repository.BrandRepository;
 import org.softuni.mobilele.repository.ModelRepository;
 import org.softuni.mobilele.service.BrandService;
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class BrandServiceImpl implements BrandService {
 
+  public static final Comparator<ModelDTO> MODEL_DTO_BY_NAME_ASC = Comparator.comparing(ModelDTO::name);
+  public static final Comparator<String> BRAND_NAME_ASC = String::compareTo;
   private final ModelRepository modelRepository;
   private final BrandRepository brandRepository;
 
@@ -25,15 +30,35 @@ public class BrandServiceImpl implements BrandService {
   @Override
   public List<BrandDTO> getAllBrands() {
 
-    return brandRepository.findAll().stream()
-            .map(brand -> new BrandDTO(
-                    brand.getBrand(),
-                    modelRepository.findAllByBrandId(brand.getId()).stream()
-                            .map(model -> new ModelDTO(model.getId(), model.getName()))
-                            .sorted(Comparator.comparing(ModelDTO::name))
-                            .collect(Collectors.toList())
-            ))
-            .sorted(Comparator.comparing(BrandDTO::name))
-            .collect(Collectors.toList());
+    return getAllBrandsSorted(BRAND_NAME_ASC, MODEL_DTO_BY_NAME_ASC);
   }
+
+  public List<BrandDTO> getAllBrandsSorted(Comparator<String> brandNameComparator,
+                                           Comparator<ModelDTO> modelDTOComparator) {
+
+    final Supplier<TreeMap<String, List<ModelDTO>>> treeMapSupplier = () ->
+            new TreeMap<>(brandNameComparator);
+
+    final Function<ModelEntity, ModelDTO> mapModelEntityToDto = (model) ->
+            new ModelDTO(model.getId(), model.getName());
+
+    final Function<List<ModelDTO>, List<ModelDTO>> getSortedList = (list) ->
+            list.stream().sorted(modelDTOComparator).toList();
+
+    return modelRepository.findAll().stream()
+            .collect(Collectors.groupingBy(
+                    model -> model.getBrand().getBrand(),
+                    treeMapSupplier,
+                    Collectors.mapping(
+                            mapModelEntityToDto,
+                            Collectors.collectingAndThen(
+                                    Collectors.toList(),
+                                    getSortedList)
+                    )))
+            .entrySet().stream()
+            .map(entry -> new BrandDTO(entry.getKey(),
+                                       entry.getValue()))
+            .toList();
+  }
+
 }
