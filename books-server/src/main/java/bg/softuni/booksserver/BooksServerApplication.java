e2d50e21411b16d322feca69a8ab35792352dd8d
rd-lachezar-balev <lachezar.balev@gmail.com>
2023-10-23 19:14:01 +0300

    Persistence layer.

---
+++ books-server/src/main/java/bg/softuni/booksserver/BooksServerApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.booksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksServerApplication.class, args);
+	}
+
+}
---
+++ books-server/src/test/java/bg/softuni/booksserver/BooksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.booksserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
