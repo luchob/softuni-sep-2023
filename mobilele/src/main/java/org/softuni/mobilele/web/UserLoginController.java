a4a0c066ce9b30f52ddb79e9298c812e5ab8f0b5
rd-lachezar-balev <lachezar.balev@gmail.com>
2023-10-25 22:27:06 +0300

    Few additional changes..

--- mobilele/src/main/java/org/softuni/mobilele/web/UserLoginController.java
+++ mobilele/src/main/java/org/softuni/mobilele/web/UserLoginController.java
@@ -1,13 +1,27 @@
 package org.softuni.mobilele.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserLoginController {
 
   @GetMapping("/users/login")
   public String login() {
+    return "auth-login";
+  }
+
+  @PostMapping("/users/login-error")
+  public String onFailure(
+      @ModelAttribute("email") String email,
+      Model model) {
+
+    model.addAttribute("email", email);
+    model.addAttribute("bad_credentials", "true");
+
     return "auth-login";
   }
 }
